1905f982190b9166b82c699e79e56acd356ec3dc
DungeonMesher <32795703+DungeonMesher@users.noreply.github.com>
2018-06-17 00:49:12 -0400

    Pseudo Code 1

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+//List of countries
+
+//Pick a random country for the player to guess
+
+//Number of allowed guesses
+
+//Store the user's guessed letters
+
+//Track the previously guessed letters
+
+//Track the remaining number of guesses
+
+//If the player guesses all the correct letters, they win
+
+//Track the total number of wins
+
+//If the players exceeds the number of guesses, they lose
+
+//Event - Listen for the keys being pressed
+
+    //Each unique guess should be added to the list of previously guessed letters
+
+    //Each correct guess should be tracked and updated in a variable
+
+    //Check to see if the player has won
+
+    //If the player has won, do something to end the game & send an alert
+
+    //Deduct incorrect guesses from the guessing allowence
+
+    //Check to see if the user has exceeded the guessing allowence
+
+    //If the player has lost, end the game & send the them an alert 
+
+
+
+
